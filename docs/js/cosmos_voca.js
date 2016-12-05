85c0ca809408c076563fa18f36834d2e2d2010d3
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-05 19:07:31 +0900

    modified page navigation behavior correctly.

--- docs/js/cosmos_voca.js
+++ docs/js/cosmos_voca.js
@@ -4,15 +4,19 @@ var storeObject = {
     selectedEpisodeTitle : ''
 };
 
-$(document).on('pagebeforeshow', '#table_of_contents', function(){       
+$(document).on('pagebeforeshow', '#table-of-contents', function(){       
     $(document).on('click', '.next-page-button', function(){
         storeObject.selectedEpisodeNo
             = this.innerText.substring(0, this.innerText.indexOf('.'));
         storeObject.selectedEpisodeTitle = this.innerText;
-        $.mobile.changePage('./voca.html');
+        $.mobile.changePage('#vocabulary');
     });    
 });
 
 $(document).on('pagebeforeshow', '#vocabulary', function(){
+    if (storeObject.selectedEpisodeTitle == '') {
+        $.mobile.changePage('#table-of-contents');
+        return;
+    }
     $('#episode-name').text(storeObject.selectedEpisodeTitle);     
 });
