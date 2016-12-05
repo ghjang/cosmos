9fc1bc7df3a40221e146b22f6f164c0e1ed360d4
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-06 00:12:05 +0900

    fixed incorrect page navigation problem.

--- docs/js/cosmos_voca.js
+++ docs/js/cosmos_voca.js
@@ -1,22 +1,29 @@
-// Store object
-var storeObject = {
+// store obj
+var g_storeObj = {
     selectedEpisodeNo : 0,
     selectedEpisodeTitle : ''
 };
 
-$(document).on('pagebeforeshow', '#table-of-contents', function(){       
+var g_tableOfContentsPageInit = false;
+
+
+$(document).on('pagebeforeshow', '#table-of-contents', function(){
+    if (g_tableOfContentsPageInit) {
+        return;
+    }       
     $(document).on('click', '.next-page-button', function(){
-        storeObject.selectedEpisodeNo
+        g_storeObj.selectedEpisodeNo
             = this.innerText.substring(0, this.innerText.indexOf('.'));
-        storeObject.selectedEpisodeTitle = this.innerText;
+        g_storeObj.selectedEpisodeTitle = this.innerText;
         $.mobile.changePage('#vocabulary');
-    });    
+    });
+    g_tableOfContentsPageInit = true;
 });
 
 $(document).on('pagebeforeshow', '#vocabulary', function(){
-    if (storeObject.selectedEpisodeTitle == '') {
+    if (g_storeObj.selectedEpisodeTitle == '') {
         $.mobile.changePage('#table-of-contents');
         return;
     }
-    $('#episode-name').text(storeObject.selectedEpisodeTitle);     
+    $('#episode-name').text(g_storeObj.selectedEpisodeTitle);     
 });
